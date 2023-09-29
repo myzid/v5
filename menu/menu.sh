b63e6c3592cba9c7eeffd1e08e5c5bd4afa37e00
myzid <143302655+myzid@users.noreply.github.com>
2023-09-29 21:09:33 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -26,28 +26,49 @@ checking_sc() {
 checking_sc
 clear
 
-rm -f /usr/bin/user
-username=$(curl https://raw.githubusercontent.com/myzid/izin/main/ip | grep $MYIP | awk '{print $2}')
-echo "$username" > /usr/bin/user
-expx=$(curl https://raw.githubusercontent.com/myzid/izin/main/ip | grep $MYIP | awk '{print $3}')
-echo "$expx" > /usr/bin/e
-# DETAIL ORDER
-username=$(cat /usr/bin/user)
-oid=$(cat /usr/bin/ver)
-exp=$(cat /usr/bin/e)
-clear
-# CERTIFICATE STATUS
-d1=$(date -d "$valid" +%s)
-d2=$(date -d "$today" +%s)
-certifacate=$(((d1 - d2) / 86400))
-# VPS Information
-DATE=$(date +'%Y-%m-%d')
-datediff() {
-    d1=$(date -d "$1" +%s)
-    d2=$(date -d "$2" +%s)
-    echo -e "$COLOR1 $NC Expiry In   : $(( (d1 - d2) / 86400 )) Days"
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/myzid/izin/main/ip > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f  /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f  /root/tmp
 }
-mai="datediff "$Exp" "$DATE""
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/myzid/izin/main/ip | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/myzid/izin/main/ip | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
 
 # =========================================
 vlx=$(grep -c -E "^#& " "/etc/xray/config.json")
@@ -275,8 +296,8 @@ echo -e "${BICyan} │  ${BICyan}[${BIWhite}07${BICyan}] BACKUP  ${BICyan}[${BIY
 echo -e "${BICyan} └─────────────────────────────────────────────────────┘${NC}"
 echo -e "        ${BICyan}┌─────────────────────────────────────┐${NC}"
 echo -e "        ${BICyan}│$NC Version       : V1.01${NC}"
-echo -e "        ${BICyan}│$NC ${GREEN}User          :\033[1;36m $username \e[0m"
-echo -e "        ${BICyan}│$NC Expired       : ${GREEN}$expx $NC"
+echo -e "        ${BICyan}│$NC ${GREEN}User          :\033[1;36m $Name \e[0m"
+echo -e "        ${BICyan}│$NC Expired       : ${GREEN}$Exp ( $Exp2 ) $NC"
 echo -e "        ${BICyan}└─────────────────────────────────────┘${NC}"
 echo
 read -p " Select menu : " opt
